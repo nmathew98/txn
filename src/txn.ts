ab90de77834afa0b7861d7e05bab8d0278f570ac
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2024-02-11 13:03:14 +1300

    fix: improve types, make onError optional, unit test and impl here

--- src/txn.ts
+++ src/txn.ts
@@ -1,22 +1,24 @@
 export interface CreateTransactionOptions {
 	[prefix: string]: {
 		onSuccess: (result: unknown) => Promise<void>;
-		onError: (error: unknown) => Promise<void>;
+		onError?: (error: unknown) => Promise<void>;
 	};
 }
 
 export interface Transaction<T> extends AsyncDisposable {
 	exec: ExecQuery;
-	resolve: ResolveTransaction<T>;
+	resolve: ResolveTransaction;
 }
 
-export interface ResolveTransaction<T = unknown> {}
+export interface ResolveTransaction {
+	<T extends (...args: any[]) => Promise<Record<string, any>> | Promise<void>>(f: T): ReturnType<typeof f>
+}
 
 export interface ExecQuery<T = unknown> {
 	(
 		key: string,
 		f: (...args: any[]) => Promise<T>,
-	): (...args: any[]) => Promise<T>;
+	): (...args: Parameters<typeof f>) => ReturnType<typeof f>;
 }
 
 export const createTransaction = <T = unknown>(
@@ -24,8 +26,8 @@ export const createTransaction = <T = unknown>(
 ): Transaction<T> => {
 	const executedQueries = new Map();
 
-	const resolve: ResolveTransaction = async (f: () => Promise<unknown>) =>
-		f();
+	const resolve: ResolveTransaction = (f) =>  f() as any;
+
 	const exec: ExecQuery =
 		(key: string, f) =>
 		async (...args: any[]) => {
@@ -41,7 +43,7 @@ export const createTransaction = <T = unknown>(
 		};
 
 	const reject = async (key: string, error: Error) => {
-		await options[key]?.onError(key);
+		await options[key]?.onError?.(key);
 
 		throw error;
 	};
@@ -60,6 +62,18 @@ export const createTransaction = <T = unknown>(
 		[Symbol.asyncDispose]: dispose,
 	};
 };
+
+// const transaction = createTransaction({
+// 	test: {
+// 		onSuccess: () => Promise.resolve(),
+// 	}
+// });
+
+// const { test } = await transaction.resolve(async () => {
+// 	const test = await transaction.exec('test', async () => { return null; })();
+
+// 	return { test };
+// });
 
 /**
  * ```typescript
